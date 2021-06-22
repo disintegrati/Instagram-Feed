5edfbaead83ab50ed03fddb4ea5abe0d266ea0f2
plcdnl <polaccodaniele@gmail.com>
2021-06-22 16:08:35 +0200

    add start script

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 3000;
 var photos = [];
 scrapeInstagram();
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/', (req, res) => {
   return res.json(photos);
 });
